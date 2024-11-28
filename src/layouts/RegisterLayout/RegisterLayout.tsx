4b424451ee940487a2d248c1d0daca5280092a20
minatisleeping <minatt2002@gmail.com>
2024-11-28 16:06:22 +0700

    config_react_router_dom

---
+++ src/layouts/RegisterLayout/RegisterLayout.tsx
@@ -0,0 +1,12 @@
+type Props = {
+  children?: React.ReactNode
+}
+
+export default function RegisterLayout({ children }: Props) {
+  return (
+    <div>
+      RegisterLayout
+      {children}
+    </div>
+  )
+}
---
+++ src/pages/useRouteElements.tsx
@@ -0,0 +1,32 @@
+import { useRoutes } from 'react-router-dom'
+import ProductList from './ProductList'
+import Login from './Login'
+import Register from './Register'
+import RegisterLayout from '../layouts/RegisterLayout'
+
+export default function useRouteElements() {
+  const routeElements = useRoutes([
+    {
+      path: '/',
+      element: <ProductList />
+    },
+    {
+      path: '/login',
+      element: (
+        <RegisterLayout>
+          <Login />
+        </RegisterLayout>
+      )
+    },
+    {
+      path: '/register',
+      element: (
+        <RegisterLayout>
+          <Register />
+        </RegisterLayout>
+      )
+    }
+  ])
+
+  return routeElements
+}
