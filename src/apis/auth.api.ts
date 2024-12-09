a49515350f54106584dc2a1bbed4b5de90de2e79
minatisleeping <minatt2002@gmail.com>
2024-12-09 17:30:34 +0700

    handle logic login..

--- src/apis/auth.api.ts
+++ src/apis/auth.api.ts
@@ -4,3 +4,7 @@ import http from '../utils/http'
 export const registerAccount = (body: { email: string; password: string }) => {
   return http.post<AuthResponse>('/registerr', body)
 }
+
+export const login = (body: { email: string; password: string }) => {
+  return http.post<AuthResponse>('/login', body)
+}
