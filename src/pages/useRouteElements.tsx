ef000d7056f292c331b738af3968fb97d5e654eb
minatisleeping <minatt2002@gmail.com>
2024-12-17 13:23:25 +0700

    setup protected route and rejected route..

--- src/pages/useRouteElements.tsx
+++ src/pages/useRouteElements.tsx
@@ -1,14 +1,26 @@
-import { useRoutes } from 'react-router-dom'
+/* eslint-disable react-refresh/only-export-components */
+import { Navigate, Outlet, useRoutes } from 'react-router'
 import MainLayout from '../layouts/MainLayout'
 import RegisterLayout from '../layouts/RegisterLayout'
 import Login from './Login'
 import ProductList from './ProductList'
 import Register from './Register'
+import Profile from './Profile'
+
+const isAuthenticated = true
+const ProtectedRoutes = () => {
+  return isAuthenticated ? <Outlet /> : <Navigate to='/login' />
+}
+
+const RejectedRoutes = () => {
+  return !isAuthenticated ? <Outlet /> : <Navigate to='/' />
+}
 
 export default function useRouteElements() {
   const routeElements = useRoutes([
     {
       path: '/',
+      index: true,
       element: (
         <MainLayout>
           <ProductList />
@@ -16,20 +28,41 @@ export default function useRouteElements() {
       )
     },
     {
-      path: '/login',
-      element: (
-        <RegisterLayout>
-          <Login />
-        </RegisterLayout>
-      )
+      path: '/',
+      element: <ProtectedRoutes />,
+      children: [
+        {
+          path: '/profile',
+          element: (
+            <MainLayout>
+              <Profile />
+            </MainLayout>
+          )
+        }
+      ]
     },
+    //* Protected routes - only accessible when authenticated
     {
-      path: '/register',
-      element: (
-        <RegisterLayout>
-          <Register />
-        </RegisterLayout>
-      )
+      path: '/',
+      element: <RejectedRoutes />,
+      children: [
+        {
+          path: '/login',
+          element: (
+            <RegisterLayout>
+              <Login />
+            </RegisterLayout>
+          )
+        },
+        {
+          path: '/register',
+          element: (
+            <RegisterLayout>
+              <Register />
+            </RegisterLayout>
+          )
+        }
+      ]
     }
   ])
 
