4f99faa8d1eceb54986aa8324a815e1cb1b1aa38
minatisleeping <minatt2002@gmail.com>
2024-12-22 09:37:29 +0700

    code logic sort product list component..

--- src/pages/ProductList/ProductList.tsx
+++ src/pages/ProductList/ProductList.tsx
@@ -5,7 +5,7 @@ import Pagination from 'src/components/Pagination'
 import useQueryParams from 'src/hooks/useQueryParams'
 import AsideFilter from 'src/pages/ProductList/AsideFilter'
 import Product from 'src/pages/ProductList/Product/Product'
-import SortProduct from 'src/pages/ProductList/SortProductList'
+import SortProductList from 'src/pages/ProductList/SortProductList'
 import { ProductListConfig } from 'src/types/product.type'
 
 export type QueryConfig = {
@@ -18,7 +18,7 @@ export default function ProductList() {
   const queryConfig: QueryConfig = omitBy(
     {
       page: queryParams.page || '1',
-      limit: queryParams.limit,
+      limit: queryParams.limit || '20',
       sort_by: queryParams.sort_by,
       exclude: queryParams.exclude,
       name: queryParams.name,
@@ -48,7 +48,7 @@ export default function ProductList() {
               <AsideFilter />
             </div>
             <div className='col-span-9'>
-              <SortProduct />
+              <SortProductList queryConfig={queryConfig} pageSize={data.data.data.pagination.page_size} />
               <div className='mt-6 grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 gap-3'>
                 {data.data.data.products.map((product) => (
                   <div className='col-span-1' key={product._id}>
